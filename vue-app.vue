7d95752ee68070b6dc570af1d9257ae86e9dcaf2
Andy Liu <andyliu1998@Andys-MBP.lan>
2023-02-20 00:32:32 -0500

    Initial commit

---
+++ vue-app.vue
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+
+new Vue({
+  el: '#app',
+  data: {
+    pageViews: 0,
+    bounces: 0,
+    bounceRate: 0
+  },
+  created() {
+    this.incrementPageViews();
+    this.calculateBounceRate();
+  },
+  methods: {
+    incrementPageViews() {
+      this.pageViews++;
+    },
+    incrementBounces() {
+      this.bounces++;
+    },
+    calculateBounceRate() {
+      window.addEventListener('beforeunload', () => {
+        this.incrementBounces();
+        this.bounceRate = Math.round((this.bounces / this.pageViews) * 100);
+      });
+    }
+  }
+});
